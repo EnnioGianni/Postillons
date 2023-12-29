705010a52c255e5511b12072d051cd37b3fc9048
EnnioGianni <gianniennio5@gmail.com>
2023-12-29 17:56:48 +0100

    '

--- JS/index.vue
+++
@@ -1,3 +0,0 @@
-<template>
-    <Header title="Lailly - Orléanais (43)" deskOpenned="13/02/1887" deskClosed="13/02/1897"></Header>
-</template>
--- JS/index2.vue
+++
@@ -1,3 +0,0 @@
-<template>
-    <Header title="Longway" deskOpenned="13/02/1887" deskClosed="13/02/1897"></Header>
-</template>
